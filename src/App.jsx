466a0fdcc86891927ad36eea79e9b9d0e49debe5
Nikita <kavtsevichn@mail.ru>
2022-08-12 16:32:28 +0200

    userslist

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,22 @@
-import React, { useState } from 'react'
-import Expenses from './components/Expenses';
-import NewExpense from './components/NewExpense';
+import React, {useState} from 'react'
+import AddUser from './components/Users/AddUser';
+import UsersList from './components/Users/UsersList'
+import './index.css';
 function App() {
 
-  const DUMY_EXPENSES = [
-    { id: 1, title: "Car Insurance", amount: 294.67, date: new Date(2019, 2, 28) },
-    { id: 2, title: "Shopping", amount: 40, date: new Date(2019, 1, 24) },
-    { id: 3, title: "House", amount: 470, date: new Date(2022, 3, 18) },
-    { id: 4, title: "Laptop", amount: 2000, date: new Date(2021, 5, 28) }
-  ];
+  const [usersList, setUsersList] = useState([])
 
-  const [expenses, setExpenses] = useState(DUMY_EXPENSES)
-
-  const addExpenseHandler = (expense) => {
-    setExpenses((prev) => {
-      return [expense, ...prev]
-    })
+  const addUserHandler = (userName, userAge) => {
+    setUsersList(prev => {
+      return [...prev, 
+        {name: userName, age: userAge, id: Math.random().toString()}]
+    });
   }
-
+  
   return (
     <div>
-      <NewExpense onAddExpense={addExpenseHandler}/>
-      <Expenses items={expenses}/>
-      
+      <AddUser onAddUser={addUserHandler}/>
+      <UsersList users={usersList}/>
     </div>
   );
 }
